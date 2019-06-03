de86c01868d085a6dcf357d1b9883b5ae7909f08
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2019-06-03 18:03:13 +0900

    Update MotivationProgram.java

--- MotivationProgram.java
+++ MotivationProgram.java
@@ -9,10 +9,10 @@ class sportsMember {
         int choiceThis = sc.nextInt();
         if(choiceThis == 1) {
             ArrayList<String> bsBall = new ArrayList<String>();
-            bsBall.add("\n(마이클 조던) : 두려움은 환상이다 \n\n");
+            bsBall.add("\n\n(마이클 조던) : 두려움은 환상이다 \n\n");
             bsBall.add("9000번 이상 슛을 놓치고.300번의 게임에서 졌다.\n 나를 믿고 맡겨진 결정골을 놓친것은 26번이다.\n 나는 지금까지 실패하고 또 실패했다.\n 그것이 내가 성공한 이유다.\n\n");
-            bsBall.add("WELCOME to NBA \n\n");
-            bsBall.add("\n(찰스 바클리) : 누군가는 영웅이 되어야 한다. 어쩌면 그 영웅이 나일지도 모른다 \n\n");
+            bsBall.add("WELCOME to NBA \n\n\n");
+            bsBall.add("(찰스 바클리) : 누군가는 영웅이 되어야 한다. 어쩌면 그 영웅이 나일지도 모른다 \n\n");
             System.out.println(bsBall);
         }
     }
