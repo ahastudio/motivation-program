2d7e468001f77a92046ca263deb5a2cd92448872
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2019-06-04 11:38:56 +0900

    Update Category.java

--- Category.java
+++ Category.java
@@ -17,7 +17,11 @@ public class Category {
     }
 
     public class BusinessCategory {
-        public BusinessCategory() { System.out.println("1.인물  2.경영이념  3.마케팅  4.재무회계  5.인사관리  6.기타");
+        public BusinessCategory() {
+            System.out.println("1.인물  2.경영이념  3.마케팅  4.재무회계  5.인사관리  6.기타");
+            View LargeSports = new View();
+            View.ViewBusiness enjoyBusiness = LargeSports.new ViewBusiness();
+            System.out.println(enjoyBusiness);
         }
     }
 
