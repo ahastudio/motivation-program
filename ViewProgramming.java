deaa05af6442d6a684f7fb60a4ab793618a60432
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2019-06-04 14:34:18 +0900

    Create ViewProgramming.java

---
+++ ViewProgramming.java
@@ -0,0 +1,22 @@
+package JavaReport;
+
+import java.util.*;
+
+public class ViewProgramming {
+    public ViewProgramming() {
+
+    }
+
+    public class ViewDeveloper {
+        public ViewDeveloper() {
+            Scanner chooseProgramming = new Scanner(System.in);
+            int selectedProgramming = chooseProgramming.nextInt();
+            if (selectedProgramming == 1) {
+                // Reference by ArrayListModule.java
+                ArrayListModule LargeModule = new ArrayListModule();
+                ArrayListModule.DeveloperModule showDeveloper = LargeModule.new DeveloperModule();
+                System.out.println(showDeveloper);
+            }
+        }
+    }
+}
