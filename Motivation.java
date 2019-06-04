8c9ff66ecc8945e07a780422dc3b3cbca2028e7c
Ashal aka JOKER <ahastudio@gmail.com>
2019-06-05 02:21:10 +0900

    Use application instance
    Create instance of Motivation class and use it.

--- Motivation.java
+++ Motivation.java
@@ -7,6 +7,11 @@ public class Motivation {
             new String[]{"sports", "business", "programming", "life"};
 
     public static void main(String[] args) {
+        Motivation app = new Motivation();
+        app.run();
+    }
+
+    public void run() {
         Menu menu = new Menu();
 
         System.out.println(menu.getPrompt(""));
