e99be0eabca58f5b18d86539cf4f27f0bc0203c8
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2019-06-04 09:56:00 +0900

    Update Motivation.java

--- Motivation.java
+++ Motivation.java
@@ -1,40 +1,6 @@
 package JavaReport;
 import java.util.*;
 
-
-class SportsCategory {
-    public SportsCategory() {
-        System.out.println("1.농구  2.야구  3.축구  4.미식축구  5.바둑  6.기타");
-        Scanner chooseSports = new Scanner(System.in);
-        int selectedSports = chooseSports.nextInt();
-        if(selectedSports == 1) {
-            ArrayList<String> basketBall = new ArrayList<String>();
-            basketBall.add("(마이클 조던) : 두려움은 환상이다 \n\n");
-            basketBall.add("9000번 이상 슛을 놓치고.300번의 게임에서 졌다.\n 나를 믿고 맡겨진 결정골을 놓친것은 26번이다.\n 나는 지금까지 실패하고 또 실패했다.\n 그것이 내가 성공한 이유다.\n\n");
-            basketBall.add("WELCOME to NBA \n\n\n");
-            basketBall.add("(찰스 바클리) : 누군가는 영웅이 되어야 한다. 어쩌면 그 영웅이 나일지도 모른다 \n\n");
-            System.out.println(basketBall);
-        }
-    }
-}
-
-class BusinessCategory {
-    public BusinessCategory() { System.out.println("1.인물  2.경영이념  3.마케팅  4.재무회계  5.인사관리  6.기타");
-    }
-}
-
-class ProgrammingCategory {
-    public ProgrammingCategory() {
-        System.out.println("1.인물  2.Java  3.JavaScript  4.node.js  5.C  6.C++");
-    }
-}
-
-class LifeCategory {
-    public LifeCategory() {
-        System.out.println("1.철학");
-    }
-}
-
 public class Motivation {
     public static void main(String[] args)  {
         System.out.println("[동기부여 프로그램] 주제별 1.스포츠  2.비즈니스  3,프로그래밍  4.인생");
@@ -42,17 +8,19 @@ public class Motivation {
         Scanner chooseFirst = new Scanner(System.in);
         int selectedFirst = chooseFirst.nextInt();
 
+        Category LargeCategory = new Category();
+
         if(selectedFirst == 1) {
-            SportsCategory instanceSports = new SportsCategory();
+            Category.SportsCategory instanceSports = LargeCategory.new SportsCategory();
             System.out.println(instanceSports);
         } else if(selectedFirst == 2) {
-            BusinessCategory instanceBusiness = new BusinessCategory();
+            Category.BusinessCategory instanceBusiness = LargeCategory.new BusinessCategory();
             System.out.println(instanceBusiness);
         } else if(selectedFirst == 3) {
-            ProgrammingCategory instanceProgramming = new ProgrammingCategory();
+            Category.ProgrammingCategory instanceProgramming = LargeCategory.new ProgrammingCategory();
             System.out.println(instanceProgramming);
         } else if(selectedFirst == 4) {
-            LifeCategory instanceLife = new LifeCategory();
+            Category.LifeCategory instanceLife = LargeCategory.new LifeCategory();
             System.out.println(instanceLife);
         }
 
