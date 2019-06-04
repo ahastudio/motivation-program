cf676aaf93ff84eb8e76dc9b0b07a4ebe558943b
Kyu Min Lee <44955076+gyumeen@users.noreply.github.com>
2019-06-04 13:52:57 +0900

    Update ViewSports.java

--- ViewSports.java
+++ ViewSports.java
@@ -11,12 +11,10 @@ public class ViewSports {
             Scanner chooseSports = new Scanner(System.in);
             int selectedSports = chooseSports.nextInt();
             if (selectedSports == 1) {
-                ArrayList<String> ArrayListBasketball = new ArrayList<String>();
-                ArrayListBasketball.add("(마이클 조던) : 두려움은 환상이다 \n\n");
-                ArrayListBasketball.add("9000번 이상 슛을 놓치고.300번의 게임에서 졌다.\n 나를 믿고 맡겨진 결정골을 놓친것은 26번이다.\n 나는 지금까지 실패하고 또 실패했다.\n 그것이 내가 성공한 이유다.\n\n");
-                ArrayListBasketball.add("WELCOME to NBA \n\n\n");
-                ArrayListBasketball.add("(찰스 바클리) : 누군가는 영웅이 되어야 한다. 어쩌면 그 영웅이 나일지도 모른다 \n\n");
-                System.out.println(ArrayListBasketball);
+                // Reference by ArrayListModule.java
+                ArrayListModule LargeModule = new ArrayListModule();
+                ArrayListModule.BasketballModule showBasketball = LargeModule.new BasketballModule();
+                System.out.println(showBasketball);
             }
         }
     }
