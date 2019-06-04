3b15a46a9e7155acc8971500c148935dc8d087a7
Ashal aka JOKER <ahastudio@gmail.com>
2019-06-05 01:59:57 +0900

    Use constants
    Remove “if” with constants.

--- Motivation.java
+++ Motivation.java
@@ -3,26 +3,22 @@ package JavaReport;
 import java.util.Scanner;
 
 public class Motivation {
+    private static final String[] SUPER_CATEGORIES =
+            new String[]{"sports", "business", "programming", "life"};
+
     public static void main(String[] args) {
         System.out.println("[동기부여 프로그램] 주제별 1.스포츠  2.비즈니스  3,프로그래밍  4.인생");
 
-        Scanner chooseFirst = new Scanner(System.in);
-        int selectedFirst = chooseFirst.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt() - 1;
 
-        Menu menu = new Menu();
-        String superCategory = "";
-
-        // Main flow control logic
-        if (selectedFirst == 1) {
-            superCategory = "sports";
-        } else if (selectedFirst == 2) {
-            superCategory = "business";
-        } else if (selectedFirst == 3) {
-            superCategory = "programming";
-        } else if (selectedFirst == 4) {
-            superCategory = "life";
+        if (number < 0 || number >= SUPER_CATEGORIES.length) {
+            System.out.println("잘못된 입력입니다.");
+            return;
         }
 
+        Menu menu = new Menu();
+        String superCategory = SUPER_CATEGORIES[number];
         Category category = menu.createCategory(superCategory);
 
         if (category == null) {
